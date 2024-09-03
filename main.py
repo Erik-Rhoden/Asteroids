647844b5b46ec581ec3ca7347b2edaaf797cd9f8
Erik-Rhoden <erikrhoden@gmail.com>
2024-09-03 19:06:05 -0400

    add player.py and circleshape.py, update constants.py and main.py

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_RADIUS)
     dt = 0
 
     while True:
@@ -15,11 +14,11 @@ def main():
             if event.type == pygame.QUIT:
                 return
             
-        pygame.Surface.fill(screen, (0, 0, 0))
+        screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
-        
-        dt = clock.tick(60) / 1000
 
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
