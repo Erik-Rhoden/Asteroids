ea8df3ae3c9f18a0a1341c4c800f3240a90fb9d0
Erik-Rhoden <erikrhoden@gmail.com>
2024-09-04 18:53:58 -0400

    add logic to kill or break up asteroids on hit

--- asteroid.py
+++ asteroid.py
@@ -1,4 +1,6 @@
+import random
 from circleshape import *
+from constants import ASTEROID_MIN_RADIUS
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
@@ -9,3 +11,21 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += self.velocity * dt
+
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        else:
+            angle = random.uniform(20, 50)
+
+            vector = self.velocity.rotate(angle)
+            neg_vector = self.velocity.rotate(-angle)
+
+            new_radius = self.radius - ASTEROID_MIN_RADIUS
+
+            pos_asteroid = Asteroid(self.position.x, self.position.y, new_radius)
+            neg_asteroid = Asteroid(self.position.x, self.position.y, new_radius)
+            
+            pos_asteroid.velocity = vector * 1.2
+            neg_asteroid.velocity = neg_vector * 1.2
--- main.py
+++ main.py
@@ -43,7 +43,7 @@ def main():
             for shot in shots:
                 if shot.collisions(asteroid):
                     shot.kill()
-                    asteroid.kill()
+                    asteroid.split()
             
         screen.fill("black")
 
